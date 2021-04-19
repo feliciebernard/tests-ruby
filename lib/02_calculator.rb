8f996efb0885794792a6bcf17d15ece270f7c8a0
feliciebernard <felicie.bernard@outlook.fr>
2021-04-19 14:22:40 +0200

    calculette achevee

---
+++ lib/02_calculator.rb
@@ -0,0 +1,29 @@
+def number
+  puts n1 = gets.chomp.to_f
+  puts n2 = gets.chomp.to_f
+end
+
+def add(n1, n2)
+  return n1 + n2
+end
+
+def subtract(n1, n2)
+  return n1 - n2
+end
+
+def sum(n1)
+  n1.inject(0){|sum, x| sum + x }
+end
+
+def multiply(n1, n2)
+  return n1 * n2
+end
+
+def power(n1, n2)
+  return n1 ** n2
+end
+
+def factorial(n1)
+  return 1 unless n1 > 0
+  n1 * factorial(n1 - 1)
+end
